d0a36ff07f863855810b7bbabe5589f7a1541fc5
rafaelbraf <rafabrunofelix@hotmail.com>
2022-11-15 15:57:33 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -16,6 +16,6 @@ function Header() {
     </header>
 
   )
-}
+};
 
-export default Header
+export default Header;
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -7,6 +7,6 @@ function NotificationButton() {
             <img src={icon} alt="Notificar" />
         </div>
     )
-}
+};
 
-export default NotificationButton
+export default NotificationButton;
